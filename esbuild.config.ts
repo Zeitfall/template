1b6d05829fa2495306188081229f1ac962b747da
Vlad Matsko <zzeitfall@gmail.com>
2022-10-23 01:17:52 -0800

    setup

---
+++ esbuild.config.ts
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv').config();
+const server = require('@compodoc/live-server');
+const esbuild = require('esbuild');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const { sassPlugin } = require('esbuild-sass-plugin');
+
+server.start({
+  port: process.env.PORT,
+  host: process.env.HOST,
+  root: './public',
+  open: true,
+  ignore: 'node_modules',
+});
+
+esbuild.build({
+  entryPoints: ['src/App.ts', 'src/assets/scss/App.scss'],
+  outdir: 'public',
+  bundle: true,
+  watch: true,
+  plugins: [
+    sassPlugin({
+      // @ts-ignore
+      async transform(source) {
+        const { css } = await postcss([autoprefixer]).process(source);
+
+        return css;
+      },
+    }),
+  ],
+});
